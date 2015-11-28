533af3afb58bc6adb076ae94da3a289d1f404186
Fabian Boulegue <fboulegue@me.com>
2015-11-28 09:21:32 +0100

    Initial Commit

---
+++ yt_trial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  yt_trial
+//
+//  Created by Fabian Boulegue on 28.11.15.
+//  Copyright © 2015 Fabian Boulegue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
